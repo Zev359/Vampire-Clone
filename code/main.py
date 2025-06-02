8070d339c2b54a8b1fef92075c007b75b8f266e6
Zev359 <ifainberg2@gmail.com>
2025-06-02 07:55:33 -0700

    Creating frects at random positions for collision testing

--- code/main.py
+++ code/main.py
@@ -14,13 +14,14 @@ class Game:
 
         # Groups
         self.all_sprites = pygame.sprite.Group()
+        self.collision_sprites = pygame.sprite.Group()
 
         # Sprites
-        self.player = Player((400, 300), self.all_sprites)
+        self.player = Player((400, 300), self.all_sprites, self.collision_sprites) #the player is not in both groups, but has access to them
         for i in range(6):
             x, y = randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)
             w, h = randint(60, 100), randint(50, 100)
-            CollisionSprite((x, y), (w, h), self.all_sprites)
+            CollisionSprite((x, y), (w, h), (self.all_sprites, self.collision_sprites))
             # Creates frect at random x,y coordinates with random w,h within set w,h limits
 
     def run(self):
